3d18d45f41011c09abe6e9eccbc34526a684c7a0
Pedro Sena <pedrosbmartinazzo@hotmail.com>
2024-06-03 20:44:26 -0300

    projeto base spring com maven

---
+++ src/main/java/com/novatech/aquamarine/AquamarineApplication.java
@@ -0,0 +1,13 @@
+package com.novatech.aquamarine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AquamarineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AquamarineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/novatech/aquamarine/AquamarineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.novatech.aquamarine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AquamarineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
