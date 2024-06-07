5e929d15595449308b977d9db1d764887d2ff9ed
Pedro Sena <pedrosbmartinazzo@hotmail.com>
2024-06-06 22:23:52 -0300

    perfis de application properties criado, falta querry personalizada

--- src/main/java/com/novatech/aquamarine/model/PollutionReport.java
+++ src/main/java/com/novatech/aquamarine/model/PollutionReport.java
@@ -11,7 +11,6 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Lob;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToOne;
-import lombok.Builder;
 import lombok.Data;
 
 
--- src/main/java/com/novatech/aquamarine/repository/EventParticipantsRepository.java
+++ src/main/java/com/novatech/aquamarine/repository/EventParticipantsRepository.java
@@ -3,7 +3,9 @@ package com.novatech.aquamarine.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.novatech.aquamarine.model.EventParticipants;
+import java.util.List;
+
 
 public interface EventParticipantsRepository extends JpaRepository<EventParticipants, Long>{
-    
+    List<EventParticipants> findByCleanUpEventEventId(Long eventId);
 }
